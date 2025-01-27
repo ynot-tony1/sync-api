34f634fee54a3a659abcf04e0dea793c5d8fbb10
ynot-tony1 <tonycowan56@gmail.com>
2025-01-27 22:51:01 +0000

    added integration tests for 'process_video' main control flow function

--- syncnet_python/detectors/s3fd/__init__.py
+++ syncnet_python/detectors/s3fd/__init__.py
@@ -20,6 +20,7 @@ class S3FD():
         print('[S3FD] loading with', self.device)
         self.net = S3FDNet(device=self.device).to(self.device)
         state_dict = torch.load(PATH_WEIGHT, map_location=self.device)
+
         self.net.load_state_dict(state_dict)
         self.net.eval()
         print('[S3FD] finished loading (%.4f sec)' % (time.time() - tstamp))
