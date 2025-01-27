e3cd490ba5f062b0094eb3459e519ec5b5968548
ynot-tony1 <tonycowan56@gmail.com>
2025-01-27 22:15:46 +0000

    added integration tests for 'process_video' main control flow function

--- api/config/settings.py
+++ api/config/settings.py
@@ -1,20 +1,24 @@
 import os
 from dotenv import load_dotenv
+import torch
 
-# determining the project root directory
+# Determine the project root directory
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 
-# loading the .env file from the project root
+# Load the .env file from the project root
 dotenv_path = os.path.join(BASE_DIR, ".env")
 load_dotenv(dotenv_path=dotenv_path)
 
-# paths for the logging Directories
+# Device configuration
+DEVICE = os.getenv("SYNCNET_DEVICE", "cuda" if torch.cuda.is_available() else "cpu")
+
+# Paths for the logging directories
 LOGS_DIR = os.getenv("LOGS_DIR", os.path.join(BASE_DIR, "api/logs/logs"))
 FINAL_LOGS_DIR = os.getenv("FINAL_LOGS_DIR", os.path.join(BASE_DIR, "api/logs/final_logs"))
 RUN_LOGS_DIR = os.getenv("RUN_LOGS_DIR", os.path.join(BASE_DIR, "api/logs/run_logs"))
 LOG_CONFIG_PATH = os.getenv("LOG_CONFIG_PATH", os.path.join(BASE_DIR, "api/config/logging.yaml"))
 
-# different processing Directories
+# Different processing directories
 FILE_HANDLING_DIR = os.getenv("FILE_HANDLING_DIR", os.path.join(BASE_DIR, "syncnet_python/file_handling"))
 TEMP_PROCESSING_DIR = os.getenv("TEMP_PROCESSING_DIR", os.path.join(FILE_HANDLING_DIR, "temp_input"))
 DATA_WORK_PYAVI_DIR = os.getenv("DATA_WORK_PYAVI_DIR", os.path.join(BASE_DIR, "syncnet_python/data/work/pyavi"))
@@ -24,10 +28,9 @@ DATA_WORK_DIR = os.getenv("DATA_WORK_DIR", os.path.join(BASE_DIR, "syncnet_pytho
 DATA_DIR = os.getenv("DATA_DIR", os.path.join(BASE_DIR, "syncnet_python/data"))
 SYNCNET_BASE_DIR = os.getenv("SYNCNET_BASE_DIR", os.path.join(BASE_DIR, "syncnet_python"))
 
-# processing Constants
+# Processing constants
 DEFAULT_MAX_ITERATIONS = int(os.getenv("DEFAULT_MAX_ITERATIONS", 10))
 DEFAULT_TOLERANCE_MS = int(os.getenv("DEFAULT_TOLERANCE_MS", 10))
 
-# test data directory
+# Test data directory
 TEST_DATA_DIR = os.path.join(BASE_DIR, "syncnet_python", "tests", "test_data")
-
