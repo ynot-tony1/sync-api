f79e35e5e3c3ec8295d9d255ba6df12daadb6b24
ynot-tony1 <tonycowan56@gmail.com>
2025-01-30 13:17:49 +0000

    adjusted cors settings and added a new cleanall bash script for ease of pushing

--- syncnet_python/detectors/s3fd/__init__.py
+++ syncnet_python/detectors/s3fd/__init__.py
@@ -1,225 +1,62 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
-# Video 25 FPS, Audio 16000HZ
-
-import torch
-import numpy
-import time, pdb, argparse, subprocess, os, math, glob
+import time
+import numpy as np
 import cv2
-import python_speech_features
+import torch
+from torchvision import transforms
+from .nets import S3FDNet
+from .box_utils import nms_
 
-from scipy import signal
-from scipy.io import wavfile
-from SyncNetModel import *
-from shutil import rmtree
+PATH_WEIGHT = './syncnet_python/detectors/s3fd/weights/sfd_face.pth'
+img_mean = np.array([104., 117., 123.])[:, np.newaxis, np.newaxis].astype('float32')
 
-# ==================== Get OFFSET ====================
 
-def calc_pdist(feat1, feat2, vshift=10):
+class S3FD():
+
+    def __init__(self, device):
+        DEVICE = torch.device('cpu')
+        tstamp = time.time()
+        self.device = DEVICE
+
+        print('[S3FD] loading with', self.device)
+        self.net = S3FDNet(device=self.device).to(self.device)
+        state_dict = torch.load(PATH_WEIGHT, map_location=self.device)
+
+        self.net.load_state_dict(state_dict)
+        self.net.eval()
+        print('[S3FD] finished loading (%.4f sec)' % (time.time() - tstamp))
     
-    win_size = vshift*2+1
+    def detect_faces(self, image, conf_th=0.8, scales=[1]):
 
-    feat2p = torch.nn.functional.pad(feat2, (0,0, vshift, vshift))
+        w, h = image.shape[1], image.shape[0]
 
-    dists = []
+        bboxes = np.empty(shape=(0, 5))
 
-    for i in range(0, len(feat1)):
-        dists.append(torch.nn.functional.pairwise_distance(
-            feat1[[i], :].repeat(win_size, 1),
-            feat2p[i:i+win_size, :]
-        ))
+        with torch.no_grad():
+            for s in scales:
+                scaled_img = cv2.resize(image, dsize=(0, 0), fx=s, fy=s, interpolation=cv2.INTER_LINEAR)
 
-    return dists
+                scaled_img = np.swapaxes(scaled_img, 1, 2)
+                scaled_img = np.swapaxes(scaled_img, 1, 0)
+                scaled_img = scaled_img[[2, 1, 0], :, :]
+                scaled_img = scaled_img.astype('float32')
+                scaled_img -= img_mean
+                scaled_img = scaled_img[[2, 1, 0], :, :]
+                x = torch.from_numpy(scaled_img).unsqueeze(0).to(self.device)
+                y = self.net(x)
 
-# ==================== MAIN DEF ====================
+                detections = y.data
+                scale = torch.Tensor([w, h, w, h])
 
-class SyncNetInstance(torch.nn.Module):
+                for i in range(detections.size(1)):
+                    j = 0
+                    while detections[0, i, j, 0] > conf_th:
+                        score = detections[0, i, j, 0]
+                        pt = (detections[0, i, j, 1:] * scale).cpu().numpy()
+                        bbox = (pt[0], pt[1], pt[2], pt[3], score)
+                        bboxes = np.vstack((bboxes, bbox))
+                        j += 1
 
-    def __init__(self, dropout=0, num_layers_in_fc_layers=1024):
-        super(SyncNetInstance, self).__init__()
-        
-        # Initialize SyncNet on CPU
-        self.__S__ = S(num_layers_in_fc_layers=num_layers_in_fc_layers).to('cpu')
-    
-    def evaluate(self, opt, videofile):
+            keep = nms_(bboxes, 0.1)
+            bboxes = bboxes[keep]
 
-        self.__S__.eval()
-
-        # ========== ==========
-        # Convert files
-        # ========== ==========
-
-        if os.path.exists(os.path.join(opt.tmp_dir, opt.reference)):
-            rmtree(os.path.join(opt.tmp_dir, opt.reference))
-
-        os.makedirs(os.path.join(opt.tmp_dir, opt.reference))
-
-        command = ("ffmpeg -y -i %s -threads 1 -f image2 %s" % (
-            videofile,
-            os.path.join(opt.tmp_dir, opt.reference, '%06d.jpg')
-        )) 
-        output = subprocess.call(command, shell=True, stdout=None)
-
-        command = ("ffmpeg -y -i %s -async 1 -ac 1 -vn -acodec pcm_s16le -ar 16000 %s" % (
-            videofile,
-            os.path.join(opt.tmp_dir, opt.reference, 'audio.wav')
-        )) 
-        output = subprocess.call(command, shell=True, stdout=None)
-        
-        # ========== ==========
-        # Load video 
-        # ========== ==========
-
-        images = []
-        
-        flist = glob.glob(os.path.join(opt.tmp_dir, opt.reference, '*.jpg'))
-        flist.sort()
-
-        for fname in flist:
-            images.append(cv2.imread(fname))
-
-        im = numpy.stack(images, axis=3)
-        im = numpy.expand_dims(im, axis=0)
-        im = numpy.transpose(im, (0,3,4,1,2))
-
-        imtv = torch.autograd.Variable(torch.from_numpy(im.astype(float)).float())
-
-        # ========== ==========
-        # Load audio
-        # ========== ==========
-
-        sample_rate, audio = wavfile.read(os.path.join(opt.tmp_dir, opt.reference, 'audio.wav'))
-        mfcc = zip(*python_speech_features.mfcc(audio, sample_rate))
-        mfcc = numpy.stack([numpy.array(i) for i in mfcc])
-
-        cc = numpy.expand_dims(numpy.expand_dims(mfcc, axis=0), axis=0)
-        cct = torch.autograd.Variable(torch.from_numpy(cc.astype(float)).float())
-
-        # ========== ==========
-        # Check audio and video input length
-        # ========== ==========
-
-        if (float(len(audio))/16000) != (float(len(images))/25):
-            print("WARNING: Audio (%.4fs) and video (%.4fs) lengths are different." % (
-                float(len(audio))/16000,
-                float(len(images))/25
-            ))
-
-        min_length = min(len(images), math.floor(len(audio)/640))
-        
-        # ========== ==========
-        # Generate video and audio feats
-        # ========== ==========
-
-        lastframe = min_length - 5
-        im_feat = []
-        cc_feat = []
-
-        tS = time.time()
-        for i in range(0, lastframe, opt.batch_size):
-            im_batch = [
-                imtv[:, :, vframe:vframe+5, :, :] 
-                for vframe in range(i, min(lastframe, i+opt.batch_size))
-            ]
-            im_in = torch.cat(im_batch, 0)
-            im_out = self.__S__.forward_lip(im_in)  
-            im_feat.append(im_out.data.cpu())
-
-            cc_batch = [
-                cct[:, :, :, vframe*4:vframe*4+20] 
-                for vframe in range(i, min(lastframe, i+opt.batch_size))
-            ]
-            cc_in = torch.cat(cc_batch, 0)
-            cc_out = self.__S__.forward_aud(cc_in)  
-            cc_feat.append(cc_out.data.cpu())
-
-        im_feat = torch.cat(im_feat, 0)
-        cc_feat = torch.cat(cc_feat, 0)
-
-        # ========== ==========
-        # Compute offset
-        # ========== ==========
-            
-        print('Compute time %.3f sec.' % (time.time() - tS))
-
-        dists = calc_pdist(im_feat, cc_feat, vshift=opt.vshift)
-        mdist = torch.mean(torch.stack(dists, 1), 1)
-
-        minval, minidx = torch.min(mdist, 0)
-
-        offset = opt.vshift - minidx
-        conf = torch.median(mdist) - minval
-
-        fdist = numpy.stack([dist[minidx].numpy() for dist in dists])
-        # fdist = numpy.pad(fdist, (3,3), 'constant', constant_values=15)
-        fconf = torch.median(mdist).numpy() - fdist
-        fconfm = signal.medfilt(fconf, kernel_size=9)
-        
-        numpy.set_printoptions(formatter={'float': '{: 0.3f}'.format})
-        print('Framewise conf: ')
-        print(fconfm)
-        print('AV offset: \t%d \nMin dist: \t%.3f\nConfidence: \t%.3f' % (offset, minval, conf))
-
-        dists_npy = numpy.array([ dist.numpy() for dist in dists ])
-        return offset.numpy(), conf.numpy(), dists_npy
-
-    def extract_feature(self, opt, videofile):
-
-        self.__S__.eval()
-        
-        # ========== ==========
-        # Load video 
-        # ========== ==========
-        cap = cv2.VideoCapture(videofile)
-
-        frame_num = 1
-        images = []
-        while frame_num:
-            frame_num += 1
-            ret, image = cap.read()
-            if ret == 0:
-                break
-
-            images.append(image)
-
-        im = numpy.stack(images, axis=3)
-        im = numpy.expand_dims(im, axis=0)
-        im = numpy.transpose(im, (0,3,4,1,2))
-
-        imtv = torch.autograd.Variable(torch.from_numpy(im.astype(float)).float())
-        
-        # ========== ==========
-        # Generate video feats
-        # ========== ==========
-
-        lastframe = len(images) - 4
-        im_feat = []
-
-        tS = time.time()
-        for i in range(0, lastframe, opt.batch_size):
-            im_batch = [
-                imtv[:, :, vframe:vframe+5, :, :] 
-                for vframe in range(i, min(lastframe, i+opt.batch_size))
-            ]
-            im_in = torch.cat(im_batch, 0)
-            im_out = self.__S__.forward_lipfeat(im_in)  
-            im_feat.append(im_out.data.cpu())
-
-        im_feat = torch.cat(im_feat, 0)
-
-        # ========== ==========
-        # Compute offset
-        # ========== ==========
-            
-        print('Compute time %.3f sec.' % (time.time() - tS))
-
-        return im_feat
-
-    def loadParameters(self, path):
-        loaded_state = torch.load(path, map_location=lambda storage, loc: storage)
-
-        self_state = self.__S__.state_dict()
-
-        for name, param in loaded_state.items():
-            if name in self_state:
-                self_state[name].copy_(param)
+        return bboxes
