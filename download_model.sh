7ed912b12c733c681b13d4025a50a0593e6f71b9
ynot-tony1 <tonycowan56@gmail.com>
2025-02-03 17:04:56 +0000

    refactored api utils and file handling logic out of syncnet_python working dir into main api dir

--- download_model.sh
+++ download_model.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-download_file "http://www.robots.ox.ac.uk/~vgg/software/lipsync/data/syncnet_v2.model" "syncnet_python/data/syncnet_v2.model"
-download_file "http://www.robots.ox.ac.uk/~vgg/software/lipsync/data/example.avi" "syncnet_python/data/example.avi"
+mkdir syncnet_python/data
+wget http://www.robots.ox.ac.uk/~vgg/software/lipsync/data/syncnet_v2.model -O syncnet_python/data/syncnet_v2.model
+wget http://www.robots.ox.ac.uk/~vgg/software/lipsync/data/example.avi -O syncnet_python/data/example.avi
 
-# Download pre-processing pipeline weights
-download_file "https://www.robots.ox.ac.uk/~vgg/software/lipsync/data/sfd_face.pth" "syncnet_python/detectors/s3fd/weights/sfd_face.pth"
+# For the pre-processing pipeline
+mkdir syncnet_python/detectors/s3fd/weights
+wget https://www.robots.ox.ac.uk/~vgg/software/lipsync/data/sfd_face.pth -O syncnet_python/detectors/s3fd/weights/sfd_face.pth
 
 # Recreate gitignored directories
 mkdir -p api/logs/final_logs
 mkdir -p api/logs/run_logs
 mkdir -p api/logs/logs
-mkdir -p syncnet_python/file_handling/temp_input
-mkdir -p syncnet_python/file_handling/final_output
+mkdir -p api/file_handling/temp_input
+mkdir -p api/file_handling/final_output
