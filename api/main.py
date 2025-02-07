8f4612e85a77b8d9c797418c16b975b028fb55e6
ynot-tony1 <tonycowan56@gmail.com>
2025-02-07 14:48:40 +0000

    refactored main.py out into seperate routing file in a new routes directory

--- api/main.py
+++ api/main.py
@@ -1,67 +1,30 @@
-import os
-import logging
-from fastapi import FastAPI, File, HTTPException, UploadFile
-from fastapi.responses import JSONResponse, FileResponse
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api.utils.api_utils import ApiUtils
-from api.config.settings import FINAL_OUTPUT_DIR, ALLOWED_LOCAL_1, ALLOWED_LOCAL_2
-from api.process_video import process_video
+from api.config.settings import ALLOWED_LOCAL_1, ALLOWED_LOCAL_2
+from api.routes.processing_routes import router as processing_router
+from api.routes.file_routes import router as file_router
+import logging
 
 logger = logging.getLogger("fastapi")
+
 app = FastAPI()
 
-# setup cors.
 origins = [
     ALLOWED_LOCAL_1,
     ALLOWED_LOCAL_2,
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  
+    allow_origins=origins, 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-@app.post("/process")
-async def process_endpoint(file: UploadFile = File(...)):
-    # save the uploaded file to a temporary location
-    input_file_path = ApiUtils.save_temp_file(file)
-    try:
-        # process the video 
-        final_shifted_output = process_video(input_file_path, file.filename)
 
-        if not final_shifted_output or not os.path.exists(final_shifted_output):
-            logger.error("processing failed. no final output generated.")
-            raise HTTPException(status_code=500, detail="processing failed.")
-        logger.info(f"file fully processed and available at: {final_shifted_output}")
-    except Exception as e:
-        logger.error(f"an error occurred: {e}")
-        raise HTTPException(status_code=500, detail="processing failed.") from e
-    finally:
-        # remove the temp file
-        os.remove(input_file_path)
+# include the routers in the app
+app.include_router(processing_router)
+app.include_router(file_router)
 
-    corrected_filename = f"corrected_{file.filename}"
-    # return the json response with the file info
-    return JSONResponse(content={
-        "filename": corrected_filename,
-        "url": f"/download/{corrected_filename}"
-    })
-
-@app.get("/download/{filename}")
-def download_file(filename: str):
-    # create the file path for the requested file
-    file_path = os.path.join(FINAL_OUTPUT_DIR, filename)
-    # check if the file exists
-    if not os.path.isfile(file_path):
-        logger.error(f"file not found: {file_path}")
-        raise HTTPException(status_code=404, detail="file not found.")
-    logger.info(f"file download successful: {file_path}")
-    # return the file as a response
-    return FileResponse(file_path, filename=filename)
-
-@app.get("/")
+@app.get("/", tags=["root"])
 def read_root():
-    # log when the root endpoint is accessed
-    logger.info("root endpoint accessed.")
     return {"message": "welcome to sync-api"}
--- download_model.sh
+++ download_model.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
+# download the model and an example clip in which the audio is 4 frames ahead of the video
 mkdir syncnet_python/data
 wget http://www.robots.ox.ac.uk/~vgg/software/lipsync/data/syncnet_v2.model -O syncnet_python/data/syncnet_v2.model
 wget http://www.robots.ox.ac.uk/~vgg/software/lipsync/data/example.avi -O syncnet_python/data/example.avi
 
-# For the pre-processing pipeline
+# for the pre-processing pipeline
 mkdir syncnet_python/detectors/s3fd/weights
 wget https://www.robots.ox.ac.uk/~vgg/software/lipsync/data/sfd_face.pth -O syncnet_python/detectors/s3fd/weights/sfd_face.pth
 
-# Recreate gitignored directories
+# recreate gitignored directories
 mkdir -p api/logs/final_logs
 mkdir -p api/logs/run_logs
 mkdir -p api/logs/logs
