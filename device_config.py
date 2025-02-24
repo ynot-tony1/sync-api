91782920b78613fa87d87bcf780f4ef979202997
ynot-tony1 <tonycowan56@gmail.com>
2025-02-24 18:28:35 +0000

    added a system check using a torch method to determine if cuda is available

--- device_config.py
+++ device_config.py
@@ -1,1 +1,8 @@
-DEVICE = "cpu"
+import torch
+
+if torch.cuda.is_available():
+    DEVICE = "cuda"
+    print("CUDA is enabled. Using GPU:", torch.cuda.get_device_name(torch.cuda.current_device()))
+else:
+    DEVICE = "cpu"
+    print("No CUDA device found. Falling back to CPU.")
